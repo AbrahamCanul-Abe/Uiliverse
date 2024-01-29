4f85beedff8617fe3baf93ee10c6b9f904238d23
AbrahamCanul-Abe <abraham23.acc@gmail.com>
2024-01-28 18:28:18 -0600

    Add screens sizes to tailwind.config.mjs and update components Update layout background color and add styles to Divider component Update Hero component with responsive styles and add image gallery

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,15 @@ export default {
 				'custom': '2px 2px black', // Ajusta los valores según tus preferencias
 			},
 		},
+		screens: {
+			'xxs': '320px',
+			'xs': '475px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		},
 	},
 	plugins: [
 	],
